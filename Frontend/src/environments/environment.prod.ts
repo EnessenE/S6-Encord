58170b60b7cefc49962bc7ee75e0e1d589697d52
Enes <mail.enes.nu@gmail.com>
2020-05-31 18:54:56 +0200

    Update environment.prod.ts

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api_base: 'apigateway.dev.encord.kn01.fhict.nl',
+  api_base: 'http://apigateway.dev.encord.kn01.fhict.nl',
 
   settings: {
     minimalpasswordlength: 6
